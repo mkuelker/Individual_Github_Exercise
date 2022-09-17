10ba35698529a08c2287f125b4294e4dc89367d6
mkuelker <61034256+mkuelker@users.noreply.github.com>
2022-09-16 22:08:50 -0700

    Added Unittest1.py
    added example unit test

---
+++ UnitTest1.py
@@ -0,0 +1,5 @@
+from MergeSort import nums_list
+
+#I made the list generate random numbers.... We'll cheese the test 
+#a little and only check the list isn't empty 
+assert len(nums_list()) != 0, "Error"
