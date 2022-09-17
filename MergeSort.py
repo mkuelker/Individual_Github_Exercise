c71e1450a099a170a4ec80249a629542e7b050db
mkuelker <61034256+mkuelker@users.noreply.github.com>
2022-09-16 20:32:22 -0700

    Add nums_list()
    Created a function that creates a random list of numbers

---
+++ MergeSort.py
@@ -0,0 +1,53 @@
+
+import numpy as np
+from random_word import RandomWords
+
+#Driver Code. Use pip install Random-Words and Numpy
+def main():
+    list = nums_list()
+    merge_sort(list)
+    print(list)
+
+#create a random list of nums using numpy
+def nums_list():
+    list = []
+    for i in range(0, 99):
+        list.append(np.random.randint(0, high=100, size=None, dtype=int))
+    return list
+    
+#simple mergeSort
+def merge_sort(list):
+    if len(list) > 1:
+        p = len(list)//2
+        r = list[:p]
+        l = list[p:]
+
+        merge_sort(r)
+        merge_sort(l)
+
+        r_idx = l_idx = idx = 0
+
+        while r_idx < len(r) and l_idx < len(l):
+            if r[r_idx] < l[l_idx]:
+                list[idx] = r[r_idx]
+                r_idx += 1
+            else:
+                list[idx] = l[l_idx]
+                l_idx += 1 
+            idx += 1
+
+        while r_idx < len(r):
+            list[idx] = r[r_idx]
+            r_idx += 1
+            idx += 1
+
+        while l_idx < len(l):
+            list[idx] = l[l_idx]
+            l_idx += 1
+            idx += 1
+
+#run. This worked in jupiter lab so hopefully it isn't bad. 
+r =  RandomWords()
+main();
+
+     
